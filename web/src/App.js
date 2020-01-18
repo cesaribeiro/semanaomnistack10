63f76a672da0a939ec9c801a9e549489c13ab653
Augusto Ribeiro <augcsr@live.com>
2020-01-18 19:32:44 -0200

    Finalizando front web

--- web/src/App.js
+++ web/src/App.js
@@ -1,116 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log();
-      },
-      {
-        timeout: 30000,
-      });
-  }, [])
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://pa1.narvii.com/6650/082a631eb2d7cbe6cb849101808e3f5d0fcb7696_128.gif" alt="" />
-              <div className="user-info">
-                <strong>Dale Cooper</strong>
-                <span>FBI, Blue Rose, Coffe Lover</span>
-              </div>
-            </header>
-            <p>Lorem Ipsum Dolot Si Amet</p>
-            <a href="https://google.com.br">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://pa1.narvii.com/6650/082a631eb2d7cbe6cb849101808e3f5d0fcb7696_128.gif" alt="" />
-              <div className="user-info">
-                <strong>Dale Cooper</strong>
-                <span>FBI, Blue Rose, Coffe Lover</span>
-              </div>
-            </header>
-            <p>Lorem Ipsum Dolot Si Amet</p>
-            <a href="https://google.com.br">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://pa1.narvii.com/6650/082a631eb2d7cbe6cb849101808e3f5d0fcb7696_128.gif" alt="" />
-              <div className="user-info">
-                <strong>Dale Cooper</strong>
-                <span>FBI, Blue Rose, Coffe Lover</span>
-              </div>
-            </header>
-            <p>Lorem Ipsum Dolot Si Amet</p>
-            <a href="https://google.com.br">Acessar Perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://pa1.narvii.com/6650/082a631eb2d7cbe6cb849101808e3f5d0fcb7696_128.gif" alt="" />
-              <div className="user-info">
-                <strong>Dale Cooper</strong>
-                <span>FBI, Blue Rose, Coffe Lover</span>
-              </div>
-            </header>
-            <p>Lorem Ipsum Dolot Si Amet</p>
-            <a href="https://google.com.br">Acessar Perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
