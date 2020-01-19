d48b6c66e882a5c6a18382a744dc664f2c9f63cc
Augusto Ribeiro <augcsr@live.com>
2020-01-19 14:55:51 -0300

    socketio

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
 const cors = require('cors');
 
-const routes = require('./routes')
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://cesaribeiro:acsr2007@cluster0-7qdkx.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
